d1182baf562f13c9d564108a51410ed9612d186a
kaushik66 <kaushik.kd2005@gmail.com>
2026-02-08 04:49:28 +0530

    Refactor: Move Firebase config to env vars

--- frontend-react/src/firebase.js
+++ frontend-react/src/firebase.js
@@ -8,14 +8,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAFR_6Cfo2UGWkfY8uaHMphVDDw9UGmXlY",
-  authDomain: "senti-bot-auth.firebaseapp.com",
-  projectId: "senti-bot-auth",
-  storageBucket: "senti-bot-auth.firebasestorage.app",
-  messagingSenderId: "390528220641",
-  appId: "1:390528220641:web:bcd981d2b2fb13d35a26a0",
-  measurementId: "G-JTNSNH1XK5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
